d2a8764cab637932899404601062066bdb9314ee
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-02-11 20:59:34 +0530

    compoenents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import Dashboard from "../components/Dashboard"
-import SendMoney from "../components/SendMoney"
-import Signin from "../components/Signin"
-import Signup from "../components/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
-
   return (
     <>
-       <BrowserRouter>
-       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<SendMoney />} />
-       </Routes>
-       </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

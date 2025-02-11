109eaee82b57f205d8e76ea1b9f83e7a01fde99a
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-02-11 10:22:54 +0530

    routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import Dashboard from "../components/Dashboard"
+import SendMoney from "../components/SendMoney"
+import Signin from "../components/Signin"
+import Signup from "../components/Signup"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+       <BrowserRouter>
+       <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+       </Routes>
+       </BrowserRouter>
+    </>
   )
 }
 
